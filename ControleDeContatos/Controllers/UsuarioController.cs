227302d41356c9298f093945686ccdcf80ca4321
Samuel Vitor <samuelsaoliveira@gmail.com>
2023-01-14 20:28:55 -0300

    Finalizado o CRUD usuário - Editar e remover

--- ControleDeContatos/Controllers/UsuarioController.cs
+++ ControleDeContatos/Controllers/UsuarioController.cs
@@ -25,6 +25,42 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
+
+        public IActionResult Editar(int id)
+        {
+            UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
+
+        public IActionResult RemoverConfirmacao(int id)
+        {
+            UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
+        public IActionResult Remover(int id)
+        {
+            try
+            {
+                bool remover = _usuarioRepositorio.Remover(id);
+                if (remover)
+                {
+                    TempData["MensagemSucesso"] = "Usuário removido com sucesso";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Usuário não removido";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Usuário não removido, mais detalhes: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
         [HttpPost]
         public IActionResult Criar(UsuarioModel contato)
         {
@@ -45,10 +81,40 @@ namespace ControleDeContatos.Controllers
                 return RedirectToAction("Index");
             }
 
-
-
-
-
         }
+
+        [HttpPost]
+        public IActionResult Alterar(UsuarioSemSenhaModel usuarioSemSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuario = null;
+                if (ModelState.IsValid)
+                {
+                    usuario = new UsuarioModel()
+                    {
+                        Id = usuarioSemSenhaModel.Id,
+                        Nome = usuarioSemSenhaModel.Nome,
+                        Login = usuarioSemSenhaModel.Login,
+                        Email = usuarioSemSenhaModel.Email,
+                        Perfil = usuarioSemSenhaModel.Perfil
+                    };
+
+                    usuario = _usuarioRepositorio.Atualizar(usuario);
+                    TempData["MensagemSucesso"] = "Usuário alterado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View("Editar", usuario);
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Desculpe, usuário não atualizado, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+
     }
 }
