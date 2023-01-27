c69bdc89d838c8d78bb3e91a8361503f76f93c0b
Samuel Vitor <samuelsaoliveira@gmail.com>
2023-01-27 20:40:25 -0300

    Criado uma tela de alterção de senha

--- ControleDeContatos/Repositorio/IUsuarioRepositorio.cs
+++ ControleDeContatos/Repositorio/IUsuarioRepositorio.cs
@@ -19,6 +19,8 @@ namespace ControleDeContatos.Repositorio
 
         UsuarioModel Atualizar(UsuarioModel usuario);
 
+        UsuarioModel AlterarSenha(AlterarSenhaModel alterarSenhaModel);
+
         bool Remover(int id);
 
 
